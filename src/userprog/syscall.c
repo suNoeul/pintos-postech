ca380e30c7e4ec948be2f5bba0d9056970993ef8
duhongk27 <duhongk27@postech.ac.kr>
2024-11-06 17:12:58 +0000

    Feat: System call of fileIO implemented

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -6,13 +6,25 @@
 #include "threads/vaddr.h"
 #include "userprog/pagedir.h"
 #include "filesys/filesys.c"
+#include "threads/synch.h"
+
+#define MAX_FD 128 /*128로 정한 이유는 없음.*/
 
 static void syscall_handler(struct intr_frame *);
 void check_address(const void *addr);
 int alloc_fdt(struct file *f);
 
+void rw_lock_acquire_read(struct rw_lock lock);
+void rw_lock_release_read(struct rw_lock lock);
+void rw_lock_acquire_write(struct rw_lock lock);
+void rw_lock_release_write(struct rw_lock lock);
+void rw_lock_init(void);
+
+struct rw_lock filesys_lock;
+
 void syscall_init(void)
 {
+  rw_lock_init();
   intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
@@ -34,7 +46,19 @@ void halt(void)
 
 void exit(int status)
 {
+  // 임시
+  struct thread *t = thread_current();
+  // 임시
+
   // 현재 프로세스 종료 = 현재 스레드 종료
+
+  /*palloc했던 fd_table 할당해제, t는 현재 실행중인 thread라고 가정했음.*/
+  if (t->fd_table != NULL)
+  {
+    palloc_free_page(t->fd_table);
+    t->fd_table = NULL;
+  }
+
   // thread_exit()
 }
 
@@ -84,29 +108,91 @@ int open(const char *file)
 
 int filesize(int fd)
 {
+
   if (fd > 1 && fd < MAX_FD)
-    return thread_current()->fd_table[fd]->inode->data.length;
+    return file_length(thread_current()->fd_table[fd]);
   return -1;
 }
 
-int read(int fd, void buffer, unsigned size)
+int read(int fd, void *buffer, unsigned size)
 {
+  int i;
+  if (fd == 0)
+  {
+    /*표준 입력*/
+    char *char_buffer = (char *)buffer;
+    for (i = 0; i < size; i++)
+    {
+      char c = input_getc();
+      char_buffer[i] = c;
+    }
+    return size;
+  }
+  else if (fd == 1)
+  {
+    /*표준 출력*/
+    return -1;
+  }
+  else if (fd > 1 && fd < MAX_FD)
+  {
+    struct file *f = thread_current()->fd_table[fd];
+    rw_lock_acquire_read(filesys_lock);
+    file_read(f, buffer, size);
+    rw_lock_release_read(filesys_lock);
+    return size;
+  }
+  return -1;
 }
 
-int write(int fd, const void buffer, unsigned size)
+int write(int fd, const void *buffer, unsigned size)
 {
+  if (fd == 0)
+  {
+    /*표준 입력*/
+    return -1;
+  }
+  else if (fd == 1)
+  {
+    /*표준 출력*/
+    putbuf((const char *)buffer, size);
+    return size;
+  }
+  else if (fd > 1 && fd < MAX_FD)
+  {
+    struct file *f = thread_current()->fd_table[fd];
+    rw_lock_acquire_write(filesys_lock);
+    file_write(f, buffer, size);
+    rw_lock_release_write(filesys_lock);
+    return size;
+  }
+  return -1;
 }
 
 void seek(int fd, unsigned position)
 {
+  if (fd > 1 && fd < MAX_FD)
+  {
+    struct file *f = thread_current()->fd_table[fd];
+    file_seek(f, position);
+  }
 }
 
 unsigned tell(int fd)
 {
+  if (fd > 1 && fd < MAX_FD)
+  {
+    struct file *f = thread_current()->fd_table[fd];
+    return file_tell(f)
+  }
+  return -1;
 }
 
 void close(int fd)
 {
+  if (fd > 1 && fd < MAX_FD)
+  {
+    thread_current()->fd_table[fd] = NULL;
+  }
 }
 
 void check_address(const void *addr)
@@ -133,4 +219,49 @@ int alloc_fdt(struct file *f)
     }
   }
   return -1;
+}
+
+void filesys_lock_init(void)
+{
+  lock_init(&filesys_lock.mutex);
+  cond_init(&filesys_lock.readers_ok);
+  cond_init(&filesys_lock.writer_ok);
+  filesys_lock.readers = 0;
+  filesys_lock.writer = false;
+}
+
+void rw_lock_acquire_read(struct rw_lock lock)
+{
+  lock_acquire(&lock.mutex);
+  while (lock.writer)
+    cond_wait(lock.readers_ok, lock.mutex);
+  lock.readers++;
+  lock_release(&lock.mutex);
+}
+
+void rw_lock_release_read(struct rw_lock lock)
+{
+  lock_acquire(&lock.mutex);
+  lock.readers--;
+  if (lock.readers == 0)
+    cond_signal(&lock.writer_ok, &lock.mutex);
+  lock_release(&lock.mutex);
+}
+
+void rw_lock_acquire_write(struct rw_lock lock)
+{
+  lock_acquire(&lock.mutex);
+  while (lock.writer || lock.readers > 0)
+    cond_wait(&lock.writer_ok, &lock.mutex);
+  lock.writer = true;
+  lock_release(&lock.mutex);
+}
+
+void rw_lock_release_write(struct rw_lock lock)
+{
+  lock_acquire(&lock.mutex);
+  lock.writer = false;
+  cond_broadcast(&lock.readers_ok, &lock.mutex);
+  cond_signal(&lock.writer_ok, &lock.mutex);
+  lock_release(&lock.mutex);
 }
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,6 +1,15 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
-void syscall_init (void);
+void syscall_init(void);
+
+struct rw_lock
+{
+    struct lock mutex;
+    struct condition readers_ok;
+    struct condition writer_ok;
+    int readers;
+    bool writer;
+};
 
 #endif /* userprog/syscall.h */
