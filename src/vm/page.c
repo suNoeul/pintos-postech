ff461e7310dd0109a7b495620f35aff70e881b02
duhongk27 <duhongk27@postech.ac.kr>
2024-12-08 20:45:43 +0000

    dudu2

--- src/vm/page.c
+++ src/vm/page.c
@@ -68,6 +68,7 @@ void spt_destructor(struct hash_elem *e, void *aux UNUSED)
             }
         }
         else if(entry->status == PAGE_SWAP) {
+            ASSERT(entry->swap_index != -1);
             swap_free_slot(entry->swap_index);
             entry->swap_index = -1;
         }
@@ -89,7 +90,7 @@ void mmt_destructor(struct hash_elem *e, void *aux UNUSED)
         if (entry->status == PAGE_SWAP)
         {
             swap_free_slot(entry->swap_index);
-            entry->swap_index = NULL;
+            entry->swap_index = -1;
         }
     }
     free(entry);
