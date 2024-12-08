41841d31eae6e6aaa06f73f67df016126cc7f8ae
duhongk27 <duhongk27@postech.ac.kr>
2024-12-08 14:48:47 +0000

    dudu1

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -118,7 +118,7 @@ static struct frame_table_entry *frame_table_find_victim(void)
         // Reference Bit와 Dirty Bit 가져오기
         if (current_entry->owner->pagedir == NULL)
             printf("dudu: %s", current_entry->owner->name);
-        // ASSERT(current_entry->owner->pagedir != NULL);
+        ASSERT(current_entry->owner->pagedir != NULL);
         // ASSERT(current_entry->owner->pagedir != NULL);
         accessed = pagedir_is_accessed(current_entry->owner->pagedir, current_entry->upage);
         dirty = pagedir_is_dirty(current_entry->owner->pagedir, current_entry->upage);
