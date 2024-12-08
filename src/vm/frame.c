723d9c2211c1bd58583721b5d8e24009fb3dca57
duhongk27 <duhongk27@postech.ac.kr>
2024-12-08 18:18:07 +0000

    dudu

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -35,9 +35,11 @@ void *frame_allocate(enum palloc_flags flags, void *upage)
     // }
     // frame_table_add_entry(frame, upage);
 
-    if(frame != NULL)
+    if (frame != NULL) {
         ASSERT(frame_table_add_entry(frame, upage));
-    else { // Frame allocation 실패 시, Evict Frame 호출       
+    }
+    else 
+    { // Frame allocation 실패 시, Evict Frame 호출       
         if (!frame_evict()) // Eviction도 실패한 경우 NULL 반환
             ASSERT(false);
         frame = palloc_get_page(flags);
