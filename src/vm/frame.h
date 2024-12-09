90e7253e24649fb485b9bf24ba48a04990888345
snow_linux <rlatnsgh0708@postech.ac.kr>
2024-12-09 11:21:22 +0000

    [All passed] swap 코드 정리

--- src/vm/frame.h
+++ src/vm/frame.h
@@ -5,18 +5,19 @@
 #include "threads/palloc.h"
 #include "vm/page.h"
 
+struct list frame_table;
+struct lock frame_lock;
+struct list_elem *hand; // frame_table 순회를 위한 커서
 struct frame_table_entry
 {
     void *frame;            // 물리 프레임 주소
     void *upage;            // 가상 메모리 주소 (User Page)
     struct thread *owner;   // 이 프레임을 소유한 스레드
     bool pinned;            // 핀 여부 (페이지 교체 방지)
+    
     struct list_elem elem;  // Frame Table 리스트 요소
 };
 
-struct list frame_table;
-struct lock frame_lock;
-struct list_elem *hand; // frame_table 순회를 위한 커서
 
 void frame_table_init(void);
 void *frame_allocate(enum palloc_flags flags, void *upage);
